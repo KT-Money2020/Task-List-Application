4f2dc28638c3444492df586768f27bdafaca4dd9
Kevin Tran <mai_le09@yahoo.com>
2019-08-17 17:01:59 -0700

    Working x icon to remove list item if clicked on

--- app.js
+++ app.js
@@ -14,31 +14,37 @@ const taskInput = document.querySelector("#task");
 loadEventListeners();
 
 function loadEventListeners(){
+    //Add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
 }
 
 function addTask(e){
     if(taskInput.value === ''){
        alert('Add a task');
        }else{
-        //Create li element
+        //Create an li element
         const li = document.createElement('li');
-        //Add class
+        //Add a class name to the list item
         li.className = 'collection-item';
         //Create text node and append to li, another specialized version of inner.html
+        //Giving the list item a value 
         li.appendChild(document.createTextNode(taskInput.value));
            
            
-        //Create new link element
+        //Creating the 'a' link element
         const link = document.createElement('a');
-        //Add class
+        //Adding class name to the link element
         link.className = 'delete-item secondary-content';
         //Add the "x" remove icon beside the values displayed
         link.innerHTML = '<i class="fa fa-remove"></i>';
            
-        //Append the link to li
+        //Appending the link to li
+        //Looks something like this <a><li></li></a>   
         li.appendChild(link);
         //Append li to ul
+        //Adds the list item to the empty box hidden within the html
         taskList.appendChild(li);
         //Clear input
         taskInput.value = '';
@@ -48,7 +54,17 @@ function addTask(e){
 }
 
 
+// Remove Task 
 
+function removeTask(e) {
+    //Targetting the a tag which is the parent element of the li item tag
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm("Are you sure?")){
+    e.target.parentElement.parentElement.remove();
+        }
+}
+
+}
 
 
 
